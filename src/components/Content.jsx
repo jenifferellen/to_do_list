0d2e7f227aaa63ca64b4a7ddc53a82dcfc5d24fc
jeniffer <jenifferellen4@gmail.com>
2022-08-03 17:26:53 -0300

    fiz besteira testando

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,34 +1,10 @@
 import { useEffect, useState } from 'react';
 import styles from './Content.module.css';
-import iconOk from '../assets copy/iconOk.svg'
-import { Trash, Check } from 'phosphor-react';
-
-export function Content({name, onDeleteTask, id, estado, funcaoDoEstado, onHandleTotalTasks, isChecked}){
-   
-    useEffect(()=>{
-        setHasBackground(isChecked)
-    },[]);
-   
+impor
     function handleDeleteTask(){
         console.log('deletar')
         onDeleteTask(id)
-    }
-
-    const[hasBackground, setHasBackground] = useState(false)
-
-    function handleChangeColor(){
-        console.log('chamando')
-        setHasBackground(!hasBackground )
-        onHandleTotalTasks(id)
-    }
-    
-
-    
-
-
-    return( 
-        <div className={styles.all}>
-           <div className={styles.banana}>
+    }a}>
                 <div className={styles.esquerda}>
                     <button onClick={handleChangeColor}  className={hasBackground ? styles.checked : styles.check}><Check hasBackground className={hasBackground ? styles.iconCheck : styles.iconCheckNo} size={18}/></button>
                      <p className={hasBackground ? styles.pYes : styles.pNo}>{name}</p>
